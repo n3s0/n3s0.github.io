a93ff18a391585a3339fa01d345e46101f66f30b
Timothy Loftus <tloftus@protonmail.com>
2024-10-01 13:33:25 -0500

    still working on qr code. unable to figure out where to put it.

--- assets/js/pageqr.js
+++
@@ -1,8 +0,0 @@
-var qrcode = new QRCode(document.getElementById("qrcode"), {
-	text: "Test QR Code",
-	width: 128,
-	height: 128,
-	colorDark : "#000000",
-	colorLight : "#ffffff",
-	correctLevel : QRCode.CorrectLevel.H
-});
---
+++ assets/js/pageqrcode.js
@@ -0,0 +1,1 @@
+new QRCode(document.getElementById("qrcode"), "{{ .Permalink | absURL }}");
